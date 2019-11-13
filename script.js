c288b5967700cf4e70680994a5fbd0bf4517f2a4
Elin Alm <elin95alm@gmail.com>
2019-11-13 16:56:26 +0100

    tablet + mobile + footer

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 window.onload = loadPage
 
 function loadPage(){
+    clickContactAndGoToFooter()
     contactMe()
     clickArrowAndGoToPresentation()
     showTextAboutBaby()
     hidetextAboutBaby()
     showTextAboutGronaBrunnen()
     hideTextAboutGronaBrunnen()
+    toggleMenu()
 }
-
 
 function contactMe(){
     let contact = document.getElementById('contact')
@@ -25,7 +26,37 @@ function contactMe(){
    
     }
 
-    )}
+)}
+
+function clickArrowAndGoToPresentation(){
+
+    let arrow = document.getElementById('arrow')
+
+    arrow.addEventListener('click', function(){
+        
+        window.scrollTo({
+            top: 2000,
+            left: 2000,
+            behavior: 'smooth'
+        });
+
+    })
+}
+
+function clickContactAndGoToFooter(){
+    
+   let contact =  document.getElementById('contact')
+
+   contact.addEventListener('click', function(){
+
+    window.scrollTo({
+        top: 2000,
+        left: 2000,
+        behavior: 'smooth'
+      });
+   })
+
+}
 
 function clickArrowAndGoToPresentation(){
 
@@ -61,13 +92,33 @@ function showTextAboutGronaBrunnen(){
 
 function hideTextAboutGronaBrunnen(){
 
-    $('#gronabrunnen').show()
-
-    $('.gronaBrunnen').mouseleave(function(){
+    $('.gronaBrunnen').mouseout(function(){
         $('#gronabrunnen').hide()
     })
 }
 
+ 
+/**
+ * Header Ipad/Iphone
+ */
+function toggleMenu(){
+
+    let hamburger = document.querySelector('.fa-bars')
+
+    let menu = document.querySelector('ul')
+    hamburger.addEventListener('click', function(){
+
+        if(!menu.style.opacity){
+            menu.style.opacity = '1'
+            menu.style.margin = '0'
+
+        }
+        else {
+            menu.style.opacity = null
+            menu.style.margin = null
+        }
+    })
+}   
 
 
 
